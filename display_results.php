9fd88a56020defbc702aec1892ef11438c0473a0
vamsi krishna surapaneni <vks35@afsconnect2.njit.edu>
2017-02-17 17:56:17 -0500

    adding the Date in the display page

--- display_results.php
+++ display_results.php
@@ -69,6 +69,8 @@
 
         <label>Future Value:</label>
         <span><?php echo $future_value_f; ?></span><br>
+
+	<p> This calculation was done on <?php echo date('m/d/Y'); ?>.</p>
     </main>
 </body>
 </html>
