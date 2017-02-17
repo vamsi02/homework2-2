944982385ae4522ba0d96545a7214fb40ba4e2aa
vamsi krishna surapaneni <vks35@afsconnect2.njit.edu>
2017-02-17 17:50:03 -0500

    Adding validation foor intrest rate to limit the entry to below 15%

--- display_results.php
+++ display_results.php
@@ -16,7 +16,9 @@
     } else if ( $interest_rate === FALSE )  {
         $error_message = 'Interest rate must be a valid number.'; 
     } else if ( $interest_rate <= 0 ) {
-        $error_message = 'Interest rate must be greater than zero.'; 
+        $error_message = 'Interest rate must be greater than zero.';
+    } else if ( $interest_rate > 15 ) {
+      	$error_message = 'Interest rate must be less than or equal to 15';
     // validate years
     } else if ( $years === FALSE ) {
         $error_message = 'Years must be a valid whole number.';
